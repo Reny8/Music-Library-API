ffdee75f6b00ba17a6e6b32810aa8359de436ca9
Reny Reyes <renyreyes8@hotmail.com>
2022-04-25 11:52:00 -0400

    Created the serializer for the Song Model

---
+++ songs/serializers.py
@@ -0,0 +1,6 @@
+from .models import Song
+from rest_framework import serializers
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['id','title','artist','album','release_date','genre']
